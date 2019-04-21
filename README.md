e105fd35bc9bf41399923cbebe1069e76da7a4d7
dieKn <dai666k@gmail.com>
2019-04-21 21:18:12 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # MyHabit
-自分の習慣アプリ(iOS版 Swift4)
+自分の習慣アプリ(iOS版 Swift4 xcode9)
---
+++ myHabit/myHabit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myHabit
+//
+//  Created by 加藤大 on 2019/04/21.
+//  Copyright © 2019 加藤大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
