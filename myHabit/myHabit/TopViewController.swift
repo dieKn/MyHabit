9e666c5a30f772bfe614f36c50f85e80de826fac
dieKn <dai666k@gmail.com>
2019-05-15 23:48:49 +0900

    scroll success

---
+++ myHabit/myHabit/TopViewController.swift
@@ -0,0 +1,56 @@
+//
+//  TopViewController.swift
+//  myHabit
+//
+//  Created by 加藤大 on 2019/05/15.
+//  Copyright © 2019 加藤大. All rights reserved.
+//
+
+import UIKit
+
+class TopViewController: UIViewController, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let titles = ["月","火","水","木","金","土","日"]
+        
+        
+        let tabLabelWidth:CGFloat = 70
+        let tabLabelHeight:CGFloat = collectionView.frame.height
+        
+        var originX:CGFloat = 0
+        for title in titles {
+            let label = UILabel()
+            label.textAlignment = .center
+            label.frame = CGRect(x:originX, y:0, width:tabLabelWidth, height:tabLabelHeight)
+            label.text = title
+            collectionView.addSubview(label)
+            originX += tabLabelWidth
+            collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        }
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath as IndexPath) as UICollectionViewCell
+        
+        // 分かりやすいように背景色を青に
+        cell.backgroundColor = UIColor.blue
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5;
+    }
+    
+    
+}
+
--- myHabit/myHabit/ViewController.swift
+++ myHabit/myHabit/ViewController.swift
@@ -8,12 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
-    @IBOutlet weak var collectionView: UICollectionView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var scrollView: UIScrollView!
+    var didPrepareMenu = false
+    let tabLabelWidth:CGFloat = 100
+    
+    
+    override func viewDidLayoutSubviews() {
+        if didPrepareMenu { return }
+        didPrepareMenu = true
+        
+        scrollView.delegate = self
+        let titles = ["月","火","水","木","金","土","日"]
+        let tabLabelHeight:CGFloat = scrollView.frame.height
+        
+        let dummyLabelWidth = scrollView.frame.size.width/2 - tabLabelWidth/2
+        let headDummyLabel = UILabel()
+        headDummyLabel.frame = CGRect(x:0, y:0, width:dummyLabelWidth, height:tabLabelHeight)
+        scrollView.addSubview(headDummyLabel)
+        
+        var originX:CGFloat = dummyLabelWidth
+        for title in titles {
+            //タブになるUILabelを作る
+            let label = UILabel()
+            label.textAlignment = .center
+            label.frame = CGRect(x:originX, y:0, width:tabLabelWidth, height:tabLabelHeight)
+            label.text = title
+            
+            //scrollViewにぺたっとする
+            scrollView.addSubview(label)
+            
+            //次のタブのx座標を用意する
+            originX += tabLabelWidth
+        }
+        let tailLabel = UILabel()
+        tailLabel.frame = CGRect(x:originX, y:0, width:dummyLabelWidth, height:tabLabelHeight)
+        scrollView.addSubview(tailLabel)
+        originX += dummyLabelWidth
+        
+        scrollView.contentSize = CGSize(width:originX, height:tabLabelHeight)
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        guard scrollView == self.scrollView else { return }
+        let index = Int((scrollView.contentOffset.x + tabLabelWidth/2) / tabLabelWidth)
+        let x = index * 100
+        UIView.animate(withDuration: 0.3, animations: {
+            scrollView.contentOffset = CGPoint(x:x, y:0)
+        })
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        guard scrollView == self.scrollView else { return }
+        let index = Int((scrollView.contentOffset.x + tabLabelWidth/2) / tabLabelWidth)
+        let x = index * 100
+        UIView.animate(withDuration: 1.0, animations: {
+            scrollView.contentOffset = CGPoint(x:x, y:0)
+        })
     }
 
     override func didReceiveMemoryWarning() {
